2a873641ccd37c42d94a182c7da0bafd8e812838
Kemal <kemalfurkanaraci@gmail.com>
2023-08-12 17:21:19 +0300

    API key added to db.php

--- files/db.php
+++ files/db.php
@@ -1,6 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
+$API_KEY= 123213232323232lkdslkdlskdslkdsldk;
 $con = mysqli_connect("localhost","test_user","123456","test");
 // Check connection
 if (mysqli_connect_errno())
